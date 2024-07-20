5112f777acd9ea2eb0c0ea6f9a102ee9d0edace6
Jot10 <111444746+Jot10@users.noreply.github.com>
2024-07-20 19:44:58 +0530

    Add files via upload

---
+++ const/color.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color appcolor= Colors.white;
+Color buttoncolor= Color.fromARGB(255, 232, 230, 230);
---
+++ const/style.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+//import 'package:youtube/const/color.dart';
+//import 'package:youtube/const/color.dart';
+
+
+
+
+const textStyleW700= TextStyle(
+  fontWeight: FontWeight.w700, 
+ );
+ const textstylegrey=TextStyle(color: Colors.grey);
